91bf359131471680d1401818951a22f9a354108e
cesarAll <cesaralcantara.rmx@gmail.com>
2021-06-05 10:14:54 -0500

    parte intermedia del proyecto

---
+++ javascript/main.js
@@ -0,0 +1,10 @@
+console.log('Hola desde jasvascript')
+
+//DOM
+
+//seleccionar el primer elemento aunque existan muchos con las mismas carcateristicas
+let container = document.querySelector('.container');
+console.log(container)
+//seleccionar todos los elementos con las mismas carcateristicas
+let links = document.querySelectorAll('a');
+console.log(links)
